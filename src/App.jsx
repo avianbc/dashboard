f6af9491de2290c74ad7ea47dbe74be5a65b077c
Brad C <brad.carey@unanet.com>
2026-01-10 15:28:58 -0500

    init project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.module.css'
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>Workout Dashboard</h1>
+      <p>Loading...</p>
+    </div>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  base: '/dashboard/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'echarts': ['echarts', 'echarts-for-react']
+        }
+      }
+    }
+  },
+  server: {
+    open: true
+  }
+})
